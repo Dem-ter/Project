aa868f446699fc3129e428c59d7ff88611edcf2a
Dem-ter <rus.utarbaev.05@mail.ru>
2023-05-02 20:32:28 +0500

    first commit

---
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+import java.sql.*;
+import com.example.demo.model.ConnectedDataBase;
+import com.example.demo.model.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/returnListDate")
+public class RestController{
+    @PostMapping()
+    public List<Date> returnListDate(@RequestBody String numberDates) throws SQLException{
+
+
+        ConnectedDataBase dates = new ConnectedDataBase();
+        dates.setConnect(ConnectedDataBase.connect("D:/Viruchaika/dates.db"));
+        List<Date> result = dates.selectId("Dates",Integer.parseInt(numberDates));
+        dates.closeConnection();
+        return result;
+    }
+}
